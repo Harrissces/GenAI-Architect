7a092dfb8bc8d2f83cbf544e6b17a5e487a07aba
Harrissces <harrissces@gmail.com>
2025-07-08 16:32:21 +0530

    Add files via upload

---
+++ Assignments/Hari.py
@@ -0,0 +1,10 @@
+# Python program to check if a number is odd or even
+
+# Get input from user
+num = int(input("Enter a number: "))
+
+# Check even or odd
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
